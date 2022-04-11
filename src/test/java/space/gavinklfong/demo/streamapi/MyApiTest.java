198d382b149fae0e449360492b818829cc3230ab
Krzysztof <krzubrycki@gmail.com>
2022-04-11 17:14:08 +0200

    wip

--- src/test/java/space/gavinklfong/demo/streamapi/MyApiTest.java
+++ src/test/java/space/gavinklfong/demo/streamapi/MyApiTest.java
@@ -13,7 +13,10 @@ import space.gavinklfong.demo.streamapi.repos.ProductRepo;
 
 import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -109,4 +112,89 @@ public class MyApiTest {
         productList.forEach(System.out::println);
     }
 
+    @Test
+    @DisplayName("Get the 3 most recent placed order")
+    public void exercise6() {
+        List<Order> orderList = orderRepo.findAll().stream()
+                .sorted(Comparator.comparing(Order::getOrderDate).reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+        orderList.forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("Get a list of products which was ordered on 15-Mar-2021")
+    public void exercise7() {
+        List<Product> productList = productRepo.findAll().stream()
+                .filter(product -> product.getOrders().stream()
+                        .anyMatch(order -> order.getOrderDate().equals(LocalDate.of(2021, 03, 15))))
+                .collect(Collectors.toList());
+        productList.forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("Calculate the total lump of all orders placed in Feb 2021")
+    public void exercise8() {
+        LocalDate endOfJan = LocalDate.of(2021, 01, 31);
+        LocalDate firstMarch = LocalDate.of(2021, 03, 01);
+        Double sum = orderRepo.findAll().stream()
+                .filter(order -> order.getOrderDate().isAfter(endOfJan))
+                .filter(order -> order.getOrderDate().isBefore(firstMarch))
+                .flatMap(order -> order.getProducts().stream())
+                .mapToDouble(Product::getPrice)
+                .sum();
+        System.out.println(sum);
+    }
+
+    @Test
+    @DisplayName("Calculate the total lump of all orders placed in Feb 2021 (using reduce with BiFunction)")
+    public void exercise8a() {
+        BiFunction<Double, Product, Double> accumulator = (acc, product) -> acc + product.getPrice();
+        LocalDate lastJan = LocalDate.of(2021, 1, 31);
+        LocalDate firstMarch = LocalDate.of(2021, 3, 1);
+        double sum = orderRepo.findAll().stream()
+                .filter(order -> order.getOrderDate().isAfter(lastJan))
+                .filter(order -> order.getOrderDate().isBefore(firstMarch))
+                .flatMap(order -> order.getProducts().stream())
+                .reduce(0D, accumulator, Double::sum);
+        System.out.println(sum);
+    }
+
+    @Test
+    @DisplayName("Calculate the average price of all orders placed on 15-Mar-2021")
+    public void exercise9() {
+        double average = orderRepo.findAll().stream()
+                .filter(order -> order.getOrderDate().equals(LocalDate.of(2021, 3, 15)))
+                .flatMap(order -> order.getProducts().stream())
+                .mapToDouble(Product::getPrice)
+                .average()
+                .getAsDouble();
+        System.out.println("Average price: " + average);
+    }
+
+    @Test
+    @DisplayName("Obtain statistics summary of all products belong to \"Books\" category")
+    public void exercise10() {
+        DoubleSummaryStatistics summaryStatistics = productRepo.findAll().stream()
+                .filter(product -> product.getCategory().equalsIgnoreCase("books"))
+                .mapToDouble(Product::getPrice)
+                .summaryStatistics();
+        System.out.println("Number of values: " + summaryStatistics.getCount() + "\n" +
+                "Sum of prices: " + summaryStatistics.getSum() + "\n" +
+                "Minimum price: " + summaryStatistics.getMin() + "\n" +
+                "Maximum price: " + summaryStatistics.getMax() + "\n" +
+                "Average price: " + summaryStatistics.getAverage()
+        );
+    }
+
+    @Test
+    @DisplayName("Obtain a mapping of order id and the order's product count")
+    public void exercise11() {
+        Map<Long, Integer> mapping = orderRepo.findAll().stream()
+                .collect(Collectors.toMap(
+                        Order::getId,
+                        order -> order.getProducts().size()
+                ));
+        System.out.println(mapping);
+    }
 }
